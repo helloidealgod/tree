49bcd006afb1eac8facea921e61c237a72f5a0e7
xiankun.jiang <xiankun.jiang@aorise.org>
2020-06-10 20:45:12 +0800

    init

---
+++ bstree.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+typedef struct BSTreeNode{
+    int data;
+    struct BSTreeNode *left;
+    struct BSTreeNode *right;
+}BSTree;
+void main(){
+	printf("hello 二叉查找树");
+}
---
+++ rbt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+struct rb_tree{
+    char red;
+    int value;
+    struct rb_tree * left;
+    struct rb_tree * right;
+};
+void main(){
+    printf("hello world");
+}
